076089d66ef3f76b0810fc45bbebd2443562fd19
rudradcruze <francisrudra@gmail.com>
2023-03-23 14:05:58 +0600

    modify course teacher

--- studentManagementSystem.sh
+++ studentManagementSystem.sh
@@ -72,17 +72,42 @@ function create_course() {
 }
 
 function modify_teacher() {
+    echo "==== Modify Courses Teacher ===="
     echo "Enter course id:"
-    read course_id
+    read user_course_id
+    echo "Enter semester(Spring-2023):"
+    read user_semester
     echo "Enter new teacher id:"
     read user_new_teacher_id
 
     return_exsist teacher $user_new_teacher_id
     reaturn_value_teacher=$?
+
     if [ $reaturn_value_teacher == 1 ]
     then
-        echo "I am exsist"
+        INPUT=course.csv
+        count=0
+        OLDIFS=$IFS
+        IFS=','
+        [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit; }
+        while read course_id course_name semester teacher_id
+        do
+            if [ "$course_id" == "$user_course_id" ] && [ "$user_semester" == $semester ]
+            then
+                echo "$course_id,$course_name,$semester,$user_new_teacher_id" >> temp_course.csv
+            else
+                echo "$course_id,$course_name,$semester,$teacher_id" >> temp_course.csv
+            fi
+        done < $INPUT
+        IFS=$OLDIFS
+        echo "Teacher susccessfully modifyed"
+    else
+        echo "Teacher dosen't exsist"
     fi
+
+    # Remove the previous file and rename the exsisting file.
+    rm course.csv
+    mv temp_course.csv course.csv
 }
 
 function view_courses() {
@@ -94,6 +119,7 @@ function view_courses() {
     OLDIFS=$IFS
     IFS=','
     [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit; }
+    [ ! -f $INPUTTEACHER ] && { echo "$INPUTTEACHER file not found"; exit; }
     while read course_id course_name semester teacher_id
     do
         count=`expr $count + 1`
@@ -165,6 +191,7 @@ do
                             echo "Course Create Successfully"
                             ;;
                         4)
+                            clear
                             view_courses
                             ;;
                         5)
@@ -174,6 +201,7 @@ do
                             ;;
                         6)
                             clear
+                            echo "==== View teacher ===="
                             cat -b teacher.csv
                             ;;
                         7)
@@ -183,9 +211,11 @@ do
                             ;;
                         8)
                             clear
-                            view_single student
+                            echo "==== View students ===="
+                            cat -b student.csv
                             ;;
                         9)
+                            echo "==== Modify Course Teacher ===="
                             modify_teacher
                             ;;
                         10)
@@ -211,8 +241,11 @@ do
                                 echo "yes you can enroll"
                             fi
                             ;;
+                        11)
+                            exit
+                            ;;
                         *)
-                            
+                            echo "Invalid input"
                             ;;
                     esac
                     # Ask user if they want to continue
@@ -228,7 +261,6 @@ do
             echo "Invalid Input"
             ;;
     esac
-
     # Ask user if they want to continue
     echo "Do you want to continue [y/n]: "
     read choice
